ac16c547199d221aca9213b015d25c09102a4500
realaxiompeptides <realaxiompeptides@gmail.com>
2026-04-13 10:27:19 -0700

    Update functions.php

--- functions.php
+++ functions.php
@@ -452,21 +452,59 @@ function axiom_add_product_from_product_page() {
 add_action('wp_ajax_axiom_add_product_from_product_page', 'axiom_add_product_from_product_page');
 add_action('wp_ajax_nopriv_axiom_add_product_from_product_page', 'axiom_add_product_from_product_page');
 
-function axiom_checkout_research_use_field($checkout) {
-    echo '<div class="axiom-checkout-research-wrap">';
+/*
+ * Force checkout field order so email and phone show at the top.
+ */
+function axiom_reorder_checkout_fields($fields) {
+    if (isset($fields['billing']['billing_email'])) {
+        $fields['billing']['billing_email']['priority'] = 10;
+        $fields['billing']['billing_email']['required'] = true;
+    }
 
-    woocommerce_form_field('axiom_research_use_ack', array(
-        'type'     => 'checkbox',
-        'class'    => array('form-row-wide'),
-        'required' => true,
-        'label'    => 'I acknowledge this order is for research use only',
-    ), $checkout->get_value('axiom_research_use_ack'));
+    if (isset($fields['billing']['billing_phone'])) {
+        $fields['billing']['billing_phone']['priority'] = 20;
+    }
 
-    echo '<p class="axiom-checkout-research-help">All products are intended strictly for laboratory, analytical, and in-vitro research use only. Not for human or veterinary consumption.</p>';
-    echo '</div>';
+    if (isset($fields['billing']['billing_first_name'])) {
+        $fields['billing']['billing_first_name']['priority'] = 30;
+    }
+
+    if (isset($fields['billing']['billing_last_name'])) {
+        $fields['billing']['billing_last_name']['priority'] = 40;
+    }
+
+    if (isset($fields['billing']['billing_country'])) {
+        $fields['billing']['billing_country']['priority'] = 50;
+    }
+
+    if (isset($fields['billing']['billing_address_1'])) {
+        $fields['billing']['billing_address_1']['priority'] = 60;
+    }
+
+    if (isset($fields['billing']['billing_address_2'])) {
+        $fields['billing']['billing_address_2']['priority'] = 70;
+    }
+
+    if (isset($fields['billing']['billing_city'])) {
+        $fields['billing']['billing_city']['priority'] = 80;
+    }
+
+    if (isset($fields['billing']['billing_state'])) {
+        $fields['billing']['billing_state']['priority'] = 90;
+    }
+
+    if (isset($fields['billing']['billing_postcode'])) {
+        $fields['billing']['billing_postcode']['priority'] = 100;
+    }
+
+    return $fields;
 }
-add_action('woocommerce_review_order_before_submit', 'axiom_checkout_research_use_field', 9);
+add_filter('woocommerce_checkout_fields', 'axiom_reorder_checkout_fields', 999);
 
+/*
+ * Validate the custom research use checkbox that is rendered
+ * inside the checkout template/research-box partial.
+ */
 function axiom_checkout_research_use_validation() {
     if (!isset($_POST['axiom_research_use_ack'])) {
         wc_add_notice('Please confirm the research use only acknowledgment before placing your order.', 'error');
