1af346217e88b8f228e9e281cd183a8b0dc1ef18
realaxiompeptides <realaxiompeptides@gmail.com>
2026-04-13 23:27:08 -0700

    Update checkout-page.js

--- assets/js/checkout-page.js
+++ assets/js/checkout-page.js
@@ -1,57 +1,102 @@
-document.addEventListener("DOMContentLoaded", function () {
-  if (typeof jQuery === "undefined") return;
+jQuery(function ($) {
+  "use strict";
 
-  const $ = jQuery;
-  const $body = $(document.body);
-  const $form = $("form.checkout");
+  var $body = $(document.body);
+  var updateTimer = null;
 
-  if (!$form.length) return;
-
-  let updateTimer = null;
-
-  function queueCheckoutUpdate(delay = 250) {
+  function triggerCheckoutUpdate() {
     clearTimeout(updateTimer);
     updateTimer = setTimeout(function () {
       $body.trigger("update_checkout");
-    }, delay);
+    }, 250);
   }
 
-  const watchedSelector = [
-    "#billing_country",
-    "#billing_state",
-    "#billing_city",
-    "#billing_postcode",
-    "#billing_address_1",
-    "#billing_address_2",
-    "#shipping_country",
-    "#shipping_state",
-    "#shipping_city",
-    "#shipping_postcode",
-    "#shipping_address_1",
-    "#shipping_address_2",
-    "#ship-to-different-address-checkbox",
-    "input.shipping_method",
-    "select.shipping_method"
-  ].join(",");
+  function requiredAddressLooksComplete() {
+    var country = $("#billing_country").val() || "";
+    var address1 = $("#billing_address_1").val() || "";
+    var city = $("#billing_city").val() || "";
+    var state = $("#billing_state").val() || "";
+    var postcode = $("#billing_postcode").val() || "";
 
-  $(document).on("change", watchedSelector, function () {
-    queueCheckoutUpdate(150);
-  });
+    return (
+      country.trim() !== "" &&
+      address1.trim() !== "" &&
+      city.trim() !== "" &&
+      state.trim() !== "" &&
+      postcode.trim() !== ""
+    );
+  }
 
-  $(document).on(
-    "input blur keyup",
-    "#billing_city, #billing_postcode, #billing_address_1, #shipping_city, #shipping_postcode, #shipping_address_1",
-    function () {
-      queueCheckoutUpdate(350);
+  function maybeTriggerAddressUpdate() {
+    if (requiredAddressLooksComplete()) {
+      triggerCheckoutUpdate();
     }
-  );
+  }
 
-  $(document).on("updated_checkout", function () {
-    // keeps custom shipping section refreshing after WooCommerce swaps fragments
-  });
+  function bindCheckoutRefresh() {
+    var selectors = [
+      "#billing_first_name",
+      "#billing_last_name",
+      "#billing_company",
+      "#billing_country",
+      "#billing_address_1",
+      "#billing_address_2",
+      "#billing_city",
+      "#billing_state",
+      "#billing_postcode",
+      "#billing_phone",
+      "#billing_email",
+      "#ship-to-different-address-checkbox",
+      "#shipping_first_name",
+      "#shipping_last_name",
+      "#shipping_company",
+      "#shipping_country",
+      "#shipping_address_1",
+      "#shipping_address_2",
+      "#shipping_city",
+      "#shipping_state",
+      "#shipping_postcode"
+    ].join(",");
 
-  // initial sync after page load
+    $(document).on("change", selectors, function () {
+      maybeTriggerAddressUpdate();
+    });
+
+    $(document).on("input blur", selectors, function () {
+      maybeTriggerAddressUpdate();
+    });
+
+    $(document).on("change", 'input[name^="shipping_method["]', function () {
+      triggerCheckoutUpdate();
+    });
+  }
+
+  function keepCustomShippingBlockFresh() {
+    $body.on("updated_checkout", function () {
+      $(".axiom-checkout-shipping-methods-fragment input.shipping_method").off("change.axiomShipping");
+
+      $(".axiom-checkout-shipping-methods-fragment input.shipping_method").on("change.axiomShipping", function () {
+        var $input = $(this);
+        var methodName = $input.attr("name");
+        var methodValue = $input.val();
+
+        $('input[name="' + methodName + '"]').prop("checked", false);
+        $('input[name="' + methodName + '"][value="' + methodValue + '"]').prop("checked", true);
+
+        triggerCheckoutUpdate();
+      });
+    });
+  }
+
+  bindCheckoutRefresh();
+  keepCustomShippingBlockFresh();
+
+  // Initial check in case fields are prefilled by browser/autofill
   setTimeout(function () {
-    $body.trigger("update_checkout");
-  }, 500);
+    maybeTriggerAddressUpdate();
+  }, 400);
+
+  setTimeout(function () {
+    maybeTriggerAddressUpdate();
+  }, 1200);
 });
