e593fcd34c7bfc159d176427058518f4a976d565
realaxiompeptides <realaxiompeptides@gmail.com>
2026-04-15 11:26:20 -0700

    Create my-account-template.php

---
+++ my-account/my-account-template.php
@@ -0,0 +1,23 @@
+<?php
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+function axiom_account_assets() {
+    if (!function_exists('is_account_page')) {
+        return;
+    }
+
+    if (is_account_page()) {
+        error_log('AXIOM TEMPLATE DIR: ' . get_template_directory());
+        error_log('AXIOM TEMPLATE URI: ' . get_template_directory_uri());
+
+        wp_enqueue_style(
+            'axiom-account',
+            get_template_directory_uri() . '/assets/css/account/account.css',
+            array('axiom-base'),
+            '1.0'
+        );
+    }
+}
+add_action('wp_enqueue_scripts', 'axiom_account_assets', 20);
