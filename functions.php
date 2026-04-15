6a69ac052d46b961bbb0744ef427ddda47b56da0
realaxiompeptides <realaxiompeptides@gmail.com>
2026-04-14 21:02:47 -0700

    Update functions.php

--- functions.php
+++ functions.php
@@ -759,3 +759,31 @@ function axiom_apply_coupon() {
 }
 add_action('wp_ajax_axiom_apply_coupon', 'axiom_apply_coupon');
 add_action('wp_ajax_nopriv_axiom_apply_coupon', 'axiom_apply_coupon');
+
+/**
+ * Force WooCommerce to use our custom thank-you templates.
+ */
+function axiom_force_woocommerce_templates( $template, $template_name, $template_path ) {
+    $theme_template = '';
+
+    switch ( $template_name ) {
+        case 'checkout/thankyou.php':
+            $theme_template = get_stylesheet_directory() . '/woocommerce/checkout/thankyou.php';
+            break;
+
+        case 'order/order-details.php':
+            $theme_template = get_stylesheet_directory() . '/woocommerce/order/order-details.php';
+            break;
+
+        case 'order/order-details-customer.php':
+            $theme_template = get_stylesheet_directory() . '/woocommerce/order/order-details-customer.php';
+            break;
+    }
+
+    if ( $theme_template && file_exists( $theme_template ) ) {
+        return $theme_template;
+    }
+
+    return $template;
+}
+add_filter( 'woocommerce_locate_template', 'axiom_force_woocommerce_templates', 20, 3 );
