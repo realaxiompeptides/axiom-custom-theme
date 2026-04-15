d6d603599357e874454673308d247438f02d6bff
realaxiompeptides <realaxiompeptides@gmail.com>
2026-04-15 10:17:03 -0700

    Create account.php

---
+++ functions/account/account.php
@@ -0,0 +1,37 @@
+<?php
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+/**
+ * Load modular function files.
+ */
+$axiom_function_files = array(
+    '/functions/core/setup.php',
+    '/functions/core/assets.php',
+
+    '/functions/shop/catalog.php',
+
+    '/functions/cart/ajax-cart.php',
+
+    '/functions/checkout/fields.php',
+    '/functions/checkout/shipping.php',
+    '/functions/checkout/coupons.php',
+
+    '/functions/thankyou/templates.php',
+    '/functions/thankyou/header.php',
+
+    '/functions/contact/contact-us.php',
+
+    '/functions/affiliate-program/affiliate-program.php',
+
+    '/functions/account/account.php',
+);
+
+foreach ($axiom_function_files as $axiom_file) {
+    $axiom_path = get_template_directory() . $axiom_file;
+
+    if (file_exists($axiom_path)) {
+        require_once $axiom_path;
+    }
+}
