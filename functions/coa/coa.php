7fca3f49d23f0e1dd40e8545e42c4463437e7708
realaxiompeptides <realaxiompeptides@gmail.com>
2026-04-15 14:50:27 -0700

    Update coa.php

--- functions/coa/coa.php
+++ functions/coa/coa.php
@@ -5,17 +5,22 @@ if (!defined('ABSPATH')) {
 
 function axiom_coa_assets() {
     if (
-        (function_exists('is_page_template') && is_page_template('coa-page/coa-template.php')) ||
         is_page('coa') ||
-        is_page('coas')
+        is_page('coas') ||
+        is_page('certificate-of-analysis') ||
+        is_page('certificates-of-analysis') ||
+        (function_exists('is_page_template') && (
+            is_page_template('coa-template.php') ||
+            is_page_template('coa-page/coa-template.php')
+        ))
     ) {
         $theme_uri = get_template_directory_uri();
 
         wp_enqueue_style(
             'axiom-coa',
             $theme_uri . '/assets/css/coa/coa.css',
-            array('axiom-base'),
-            '1.0'
+            array(),
+            '1.3'
         );
 
         wp_enqueue_script(
@@ -27,7 +32,7 @@ function axiom_coa_assets() {
         );
     }
 }
-add_action('wp_enqueue_scripts', 'axiom_coa_assets', 20);
+add_action('wp_enqueue_scripts', 'axiom_coa_assets', 99);
 
 function axiom_get_product_coa_data($product) {
     if (!$product || !is_a($product, 'WC_Product')) {
