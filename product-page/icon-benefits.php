442944bd479c4f0d3be0dc520d9bf808e91ffb4b
realaxiompeptides <realaxiompeptides@gmail.com>
2026-04-16 09:18:54 -0700

    Update icon-benefits.php

--- product-page/icon-benefits.php
+++ product-page/icon-benefits.php
@@ -3,7 +3,7 @@
     <div class="product-icon-benefit-icon product-compliance-icon">
       <i class="fa-solid fa-flask-vial"></i>
     </div>
-    <div class="product-icon-benefit-text product-compliance-text">
+    <div class="product-icon-benefit-copy product-compliance-text">
       <p>Products are handled under controlled laboratory conditions.</p>
     </div>
   </div>
@@ -12,7 +12,7 @@
     <div class="product-icon-benefit-icon product-compliance-icon">
       <i class="fa-solid fa-microscope"></i>
     </div>
-    <div class="product-icon-benefit-text product-compliance-text">
+    <div class="product-icon-benefit-copy product-compliance-text">
       <p>Intended exclusively for research and analytical use.</p>
     </div>
   </div>
@@ -21,7 +21,7 @@
     <div class="product-icon-benefit-icon product-compliance-icon">
       <i class="fa-solid fa-circle-exclamation"></i>
     </div>
-    <div class="product-icon-benefit-text product-compliance-text">
+    <div class="product-icon-benefit-copy product-compliance-text">
       <p>No usage instructions, dosing guidance, or outcome claims are provided.</p>
     </div>
   </div>
