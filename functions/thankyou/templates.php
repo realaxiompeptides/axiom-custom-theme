9fb20b22eb8e9bc4729cd08a4f0a1bd6ebd01b83
realaxiompeptides <realaxiompeptides@gmail.com>
2026-04-16 19:41:36 -0700

    Update templates.php

--- functions/thankyou/templates.php
+++ functions/thankyou/templates.php
@@ -27,38 +27,3 @@ function axiom_force_woocommerce_templates($template, $template_name, $template_
     return $template;
 }
 add_filter('woocommerce_locate_template', 'axiom_force_woocommerce_templates', 20, 3);
-
-function axiom_force_custom_thankyou_sections() {
-    if (is_admin()) {
-        return;
-    }
-
-    remove_action('woocommerce_thankyou', 'woocommerce_order_details_table', 10);
-    add_action('woocommerce_thankyou', 'axiom_render_custom_thankyou_sections', 10);
-}
-add_action('wp', 'axiom_force_custom_thankyou_sections', 20);
-
-function axiom_render_custom_thankyou_sections($order_id) {
-    if (!$order_id) {
-        return;
-    }
-
-    $order = wc_get_order($order_id);
-
-    if (!$order) {
-        return;
-    }
-
-    axiom_render_custom_thankyou_header($order_id);
-
-    $order_details_template = get_stylesheet_directory() . '/woocommerce/order/order-details.php';
-    $customer_details_template = get_stylesheet_directory() . '/woocommerce/order/order-details-customer.php';
-
-    if (file_exists($order_details_template)) {
-        include $order_details_template;
-    }
-
-    if (file_exists($customer_details_template)) {
-        include $customer_details_template;
-    }
-}
