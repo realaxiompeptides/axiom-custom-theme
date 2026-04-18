647dceebfbf6525f1ebc6284b2b1cd39c525edbd
realaxiompeptides <realaxiompeptides@gmail.com>
2026-04-17 21:52:41 -0700

    Update default-endpoint.php

--- functions/account/default-endpoint.php
+++ functions/account/default-endpoint.php
@@ -18,9 +18,12 @@ function axiom_force_my_account_dashboard_endpoint() {
     $account_page_id = function_exists('wc_get_page_id') ? wc_get_page_id('myaccount') : 0;
     $account_slug    = $account_page_id > 0 ? trim(get_page_uri($account_page_id), '/') : 'my-account';
 
-    if ($current_request === $account_slug) {
-        wp_safe_redirect(wc_get_page_permalink('myaccount'));
-        exit;
+    /*
+     * Do nothing on the base My Account page.
+     * WooCommerce will load the dashboard there by default.
+     */
+    if ($current_request === $account_slug || $current_request === '') {
+        return;
     }
 }
 add_action('template_redirect', 'axiom_force_my_account_dashboard_endpoint', 1);
