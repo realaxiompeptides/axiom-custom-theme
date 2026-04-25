e68e9de86f05b01cdd4925d043bc4976e3032ae4
realaxiompeptides <realaxiompeptides@gmail.com>
2026-04-25 09:14:03 -0700

    Create email-footer.php

---
+++ woocommerce/emails/email-footer.php
@@ -0,0 +1,26 @@
+<?php defined('ABSPATH') || exit; ?>
+
+</td>
+</tr>
+
+<tr>
+<td align="center" style="padding:0 34px 34px;">
+    <div style="border-top:1px solid #26344f;padding-top:24px;">
+        <p style="margin:0 0 10px;color:#9db7ff;font-size:13px;font-weight:800;line-height:1.6;text-align:center;">
+            Lab-tested products • USA fulfilled • Research use only
+        </p>
+        <p style="margin:0;color:#94a3b8;font-size:12px;line-height:1.7;text-align:center;">
+            Axiom Peptides<br>
+            support@axiomresearch.shop<br>
+            axiomresearch.shop
+        </p>
+    </div>
+</td>
+</tr>
+
+</table>
+</td>
+</tr>
+</table>
+</body>
+</html>
