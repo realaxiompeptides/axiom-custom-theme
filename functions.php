f13e1c19663234d944f44e6f41109620a625dd48
realaxiompeptides <realaxiompeptides@gmail.com>
2026-04-25 14:56:14 -0700

    Update functions.php

--- functions.php
+++ functions.php
@@ -26,7 +26,6 @@ $axiom_function_files = array(
     '/functions/checkout/coupons.php',
     '/functions/checkout/payment-discounts.php',
     '/functions/checkout/card-payment-notice.php',
-    '/functions/checkout/payment-instruction-emails.php',
     '/functions/checkout/manual-payment-instructions-email.php',
     '/functions/checkout/kit-crypto-only.php',
     '/functions/checkout/checkout-addons.php',
