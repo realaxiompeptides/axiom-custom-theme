aba855f44365af39a0e267a8b834da004a4f4cb3
realaxiompeptides <realaxiompeptides@gmail.com>
2026-04-27 22:01:02 -0700

    Update floating-vials.php

--- floating-vials.php
+++ floating-vials.php
@@ -6,6 +6,13 @@
 if (!defined('ABSPATH')) exit;
 
 get_header();
+
+$random_products = wc_get_products(array(
+    'status'  => 'publish',
+    'limit'   => 5,
+    'orderby' => 'rand',
+    'return'  => 'objects',
+));
 ?>
 
 <main class="axiom-floating-test-page">
@@ -15,22 +22,10 @@ get_header();
         <div class="axiom-floating-bg-glow"></div>
 
         <div class="axiom-floating-stats">
-            <div>
-                <strong>99%+</strong>
-                <span>Purity Guaranteed</span>
-            </div>
-            <div>
-                <strong>3rd Party</strong>
-                <span>Lab Tested</span>
-            </div>
-            <div>
-                <strong>Same Day</strong>
-                <span>Fulfillment</span>
-            </div>
-            <div>
-                <strong>Discreet</strong>
-                <span>Packaging</span>
-            </div>
+            <div><strong>99%+</strong><span>Purity Guaranteed</span></div>
+            <div><strong>3rd Party</strong><span>Lab Tested</span></div>
+            <div><strong>Same Day</strong><span>Fulfillment</span></div>
+            <div><strong>Discreet</strong><span>Packaging</span></div>
         </div>
 
         <p class="axiom-floating-proof">
@@ -39,25 +34,28 @@ get_header();
 
         <div class="axiom-vial-stage">
 
-            <img class="axiom-floating-vial vial-1"
-                 src="<?php echo esc_url(get_template_directory_uri() . '/assets/images/vials/vial-1.png'); ?>"
-                 alt="Research vial">
+            <?php
+            $i = 1;
 
-            <img class="axiom-floating-vial vial-2"
-                 src="<?php echo esc_url(get_template_directory_uri() . '/assets/images/vials/vial-2.png'); ?>"
-                 alt="Research vial">
+            foreach ($random_products as $product) :
+                $image_id  = $product->get_image_id();
+                $image_url = $image_id ? wp_get_attachment_image_url($image_id, 'large') : wc_placeholder_img_src();
 
-            <img class="axiom-floating-vial vial-3"
-                 src="<?php echo esc_url(get_template_directory_uri() . '/assets/images/vials/vial-3.png'); ?>"
-                 alt="Research vial">
+                if (!$image_url) {
+                    continue;
+                }
+            ?>
+                <a class="axiom-floating-vial vial-<?php echo esc_attr($i); ?>"
+                   href="<?php echo esc_url(get_permalink($product->get_id())); ?>"
+                   aria-label="<?php echo esc_attr($product->get_name()); ?>">
 
-            <img class="axiom-floating-vial vial-4"
-                 src="<?php echo esc_url(get_template_directory_uri() . '/assets/images/vials/vial-4.png'); ?>"
-                 alt="Research vial">
-
-            <img class="axiom-floating-vial vial-5"
-                 src="<?php echo esc_url(get_template_directory_uri() . '/assets/images/vials/vial-5.png'); ?>"
-                 alt="Research vial">
+                    <img src="<?php echo esc_url($image_url); ?>"
+                         alt="<?php echo esc_attr($product->get_name()); ?>">
+                </a>
+            <?php
+                $i++;
+            endforeach;
+            ?>
 
         </div>
 
