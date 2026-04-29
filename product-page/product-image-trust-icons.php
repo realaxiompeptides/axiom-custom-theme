3353c6b120c67f4afe0b1b7c73827abf98077051
realaxiompeptides <realaxiompeptides@gmail.com>
2026-04-28 21:04:05 -0700

    Create product-image-trust-icons.php

---
+++ product-page/product-image-trust-icons.php
@@ -0,0 +1,22 @@
+<?php
+if (!defined('ABSPATH')) {
+    exit;
+}
+?>
+
+<div class="axiom-product-image-trust-icons">
+    <div class="axiom-product-image-trust-item">
+        <i class="fa-solid fa-check"></i>
+        <span>99%+ Purity</span>
+    </div>
+
+    <div class="axiom-product-image-trust-item">
+        <i class="fa-solid fa-clipboard-check"></i>
+        <span>COA Included</span>
+    </div>
+
+    <div class="axiom-product-image-trust-item">
+        <i class="fa-solid fa-flag-usa"></i>
+        <span>U.S. Shipped</span>
+    </div>
+</div>
