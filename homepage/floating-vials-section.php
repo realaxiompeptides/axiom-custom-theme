e9c24b3d425b8620da89e7a119b476dbed6c0228
realaxiompeptides <realaxiompeptides@gmail.com>
2026-05-10 14:56:47 -0700

    Update floating-vials-section.php

--- homepage/floating-vials-section.php
+++ homepage/floating-vials-section.php
@@ -3,32 +3,47 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
-$theme_uri = get_template_directory_uri();
+$theme_uri  = get_template_directory_uri();
+$theme_path = get_template_directory();
+
+function axiom_floating_vial_image_url($relative_path) {
+    $theme_uri  = get_template_directory_uri();
+    $theme_path = get_template_directory();
+
+    $file_path = $theme_path . $relative_path;
+    $file_url  = $theme_uri . $relative_path;
+
+    if (file_exists($file_path)) {
+        return $file_url . '?v=' . filemtime($file_path);
+    }
+
+    return $file_url . '?v=' . time();
+}
 
 $axiom_floating_vials = array(
     array(
         'class' => 'axiom-vial-float--one',
-        'src'   => $theme_uri . '/assets/images/floating-vials/ghk-cu-50mg.PNG',
+        'src'   => axiom_floating_vial_image_url('/assets/images/floating-vials/ghk-cu-50mg.PNG'),
         'alt'   => 'GHK-Cu 50mg',
     ),
     array(
         'class' => 'axiom-vial-float--two',
-        'src'   => $theme_uri . '/assets/images/floating-vials/glp-3-rt-60mg.PNG',
+        'src'   => axiom_floating_vial_image_url('/assets/images/floating-vials/glp-3-rt-60mg.PNG'),
         'alt'   => 'GLP-3 RT 60mg',
     ),
     array(
         'class' => 'axiom-vial-float--three',
-        'src'   => $theme_uri . '/assets/images/floating-vials/5-amino-1mq-5mg.PNG',
+        'src'   => axiom_floating_vial_image_url('/assets/images/floating-vials/5-amino-1mq-5mg.PNG'),
         'alt'   => '5-Amino 1MQ 5mg',
     ),
     array(
         'class' => 'axiom-vial-float--four',
-        'src'   => $theme_uri . '/assets/images/floating-vials/mt1-10mg.PNG',
+        'src'   => axiom_floating_vial_image_url('/assets/images/floating-vials/mt1-10mg.PNG'),
         'alt'   => 'MT-1 10mg',
     ),
     array(
         'class' => 'axiom-vial-float--five',
-        'src'   => $theme_uri . '/assets/images/floating-vials/nad-500mg.PNG',
+        'src'   => axiom_floating_vial_image_url('/assets/images/floating-vials/nad-500mg.PNG'),
         'alt'   => 'NAD+ 500mg',
     ),
 );
