ff3f712071b4c0b7676219e4d9217408d39cc19f
realaxiompeptides <realaxiompeptides@gmail.com>
2026-05-10 22:07:57 -0700

    Update single-product.php

--- single-product.php
+++ single-product.php
@@ -7,19 +7,7 @@ if (have_posts()) :
     while (have_posts()) :
         the_post();
 
-        $product = function_exists('wc_get_product') ? wc_get_product(get_the_ID()) : null;
-
-        if ($product && has_term('kits', 'product_cat', $product->get_id())) {
-            $kit_template = get_template_directory() . '/product-page/product-page-kit.php';
-
-            if (file_exists($kit_template)) {
-                include $kit_template;
-            } else {
-                include get_template_directory() . '/product-page/product-page.php';
-            }
-        } else {
-            include get_template_directory() . '/product-page/product-page.php';
-        }
+        include get_template_directory() . '/product-page/product-page.php';
 
     endwhile;
 endif;
