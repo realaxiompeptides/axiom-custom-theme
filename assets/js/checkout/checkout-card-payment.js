f54b59b6d17d9cc88b2b15d1d3179e40b48d011b
realaxiompeptides <realaxiompeptides@gmail.com>
2026-05-25 21:00:14 -0700

    Update checkout-card-payment.js

--- assets/js/checkout/checkout-card-payment.js
+++ assets/js/checkout/checkout-card-payment.js
@@ -7,15 +7,7 @@
     return String(value || '').replace(/\s+/g, ' ').trim().toLowerCase();
   }
 
-  function getThemeUrl() {
-    if (window.AXIOM_THEME && window.AXIOM_THEME.themeUrl) {
-      return String(window.AXIOM_THEME.themeUrl).replace(/\/$/, '');
-    }
-
-    return '';
-  }
-
-  function isQuiklieCardMethod($method) {
+  function isCardPaymentMethod($method) {
     if (!$method || !$method.length) {
       return false;
     }
@@ -23,12 +15,9 @@
     var methodClass = normalizeText($method.attr('class'));
     var labelText = normalizeText($method.children('label').first().text());
     var inputValue = normalizeText($method.children('input.input-radio, input[type="radio"]').first().val());
-    var imageSrc = '';
-
-    $method.children('label').first().find('img').each(function () {
-      imageSrc += ' ' + normalizeText($(this).attr('src'));
-      imageSrc += ' ' + normalizeText($(this).attr('alt'));
-    });
+    var labelImages = normalizeText($method.children('label').first().find('img').map(function () {
+      return ($(this).attr('src') || '') + ' ' + ($(this).attr('alt') || '');
+    }).get().join(' '));
 
     return (
       methodClass.indexOf('quik') !== -1 ||
@@ -36,98 +25,72 @@
       methodClass.indexOf('quiklie') !== -1 ||
       methodClass.indexOf('qpay') !== -1 ||
       methodClass.indexOf('lupa') !== -1 ||
-
-      labelText.indexOf('card') !== -1 ||
+      methodClass.indexOf('merchant') !== -1 ||
+      methodClass.indexOf('card') !== -1 ||
+      labelText.indexOf('pay by card') !== -1 ||
       labelText.indexOf('credit') !== -1 ||
       labelText.indexOf('debit') !== -1 ||
-      labelText.indexOf('pay by card') !== -1 ||
-
       inputValue.indexOf('quik') !== -1 ||
       inputValue.indexOf('quick') !== -1 ||
       inputValue.indexOf('quiklie') !== -1 ||
-      inputValue.indexOf('qpay') !== -1 ||
       inputValue.indexOf('lupa') !== -1 ||
       inputValue.indexOf('card') !== -1 ||
-
-      imageSrc.indexOf('quik') !== -1 ||
-      imageSrc.indexOf('quick') !== -1 ||
-      imageSrc.indexOf('quiklie') !== -1 ||
-      imageSrc.indexOf('lupa') !== -1
+      labelImages.indexOf('quik') !== -1 ||
+      labelImages.indexOf('quick') !== -1 ||
+      labelImages.indexOf('lupa') !== -1
     );
   }
 
   function getCardPaymentMethod() {
-    var $methods = $('.woocommerce-checkout #payment ul.payment_methods > li');
-
-    var $matched = $methods.filter(function () {
-      return isQuiklieCardMethod($(this));
+    return $('.woocommerce-checkout #payment ul.payment_methods > li').filter(function () {
+      return isCardPaymentMethod($(this));
     }).first();
-
-    return $matched;
   }
 
   function buildCardPanel() {
-    var themeUrl = getThemeUrl();
-    var cardLogoUrl = themeUrl ? themeUrl + '/assets/images/card-checkout.PNG' : '';
-
-    var logoHtml = cardLogoUrl
-      ? '<img class="axiom-card-processor-logo" src="' + cardLogoUrl + '" alt="Secure card payment" loading="lazy">'
-      : '<i class="fa-solid fa-credit-card"></i>';
-
     return $(
       [
         '<div class="' + CARD_PANEL_CLASS + '" data-axiom-card-upgrade="1">',
 
-          '<div class="axiom-card-upgrade-head">',
-            '<div class="axiom-card-upgrade-title">',
-              '<span class="axiom-card-upgrade-icon" aria-hidden="true">',
-                logoHtml,
-              '</span>',
-              '<div>',
-                '<strong>Secure Card Checkout</strong>',
-                '<span>Fast encrypted payment with instant authorization.</span>',
-              '</div>',
+          '<div class="axiom-card-head">',
+            '<span class="axiom-card-icon" aria-hidden="true">',
+              '<i class="fa-solid fa-credit-card"></i>',
+            '</span>',
+            '<div class="axiom-card-copy">',
+              '<strong>Secure Card Checkout</strong>',
+              '<span>Fast encrypted payment with instant authorization.</span>',
             '</div>',
-            '<span class="axiom-card-upgrade-badge">Card Payment</span>',
           '</div>',
 
-          '<div class="axiom-card-brand-row" aria-label="Accepted card brands">',
-            '<span class="axiom-card-brand axiom-card-brand-visa" aria-label="Visa">',
-              '<i class="fa-brands fa-cc-visa"></i>',
-            '</span>',
-            '<span class="axiom-card-brand axiom-card-brand-mastercard" aria-label="Mastercard">',
-              '<i class="fa-brands fa-cc-mastercard"></i>',
-            '</span>',
-            '<span class="axiom-card-brand axiom-card-brand-amex" aria-label="American Express">',
-              '<i class="fa-brands fa-cc-amex"></i>',
-            '</span>',
-            '<span class="axiom-card-brand axiom-card-brand-discover" aria-label="Discover">',
-              '<i class="fa-brands fa-cc-discover"></i>',
-            '</span>',
+          '<div class="axiom-card-brands" aria-label="Accepted card brands">',
+            '<span class="axiom-card-brand"><i class="fa-brands fa-cc-visa"></i></span>',
+            '<span class="axiom-card-brand"><i class="fa-brands fa-cc-mastercard"></i></span>',
+            '<span class="axiom-card-brand"><i class="fa-brands fa-cc-amex"></i></span>',
+            '<span class="axiom-card-brand"><i class="fa-brands fa-cc-discover"></i></span>',
           '</div>',
 
-          '<div class="axiom-card-descriptor-box">',
+          '<div class="axiom-card-notice">',
             '<i class="fa-solid fa-circle-info"></i>',
             '<div>',
               '<strong>Important card payment notice</strong>',
-              '<span>Charges on your card statement will appear as <b>LUPA GROUP</b>. Please ensure international payments are enabled on your card to avoid transaction failure.</span>',
+              '<span>Your card statement will show <b>LUPA GROUP</b>. Please make sure international transactions are enabled on your card before placing your order.</span>',
             '</div>',
           '</div>',
 
-          '<div class="axiom-card-mini-grid">',
-            '<div class="axiom-card-mini-card">',
+          '<div class="axiom-card-grid">',
+            '<div class="axiom-card-benefit">',
               '<i class="fa-solid fa-lock"></i>',
               '<strong>Encrypted checkout</strong>',
               '<span>Your payment details are processed securely.</span>',
             '</div>',
 
-            '<div class="axiom-card-mini-card">',
+            '<div class="axiom-card-benefit">',
               '<i class="fa-solid fa-bolt"></i>',
               '<strong>Instant authorization</strong>',
               '<span>Orders can move forward faster after approval.</span>',
             '</div>',
 
-            '<div class="axiom-card-mini-card">',
+            '<div class="axiom-card-benefit">',
               '<i class="fa-solid fa-shield-halved"></i>',
               '<strong>Bank-level security</strong>',
               '<span>Protected card submission through our processor.</span>',
@@ -139,7 +102,7 @@
     );
   }
 
-  function removeDuplicateCardPanels($cardMethod) {
+  function removeDuplicatePanels($cardMethod) {
     $('.' + CARD_PANEL_CLASS).each(function () {
       var $panel = $(this);
 
@@ -155,32 +118,43 @@
     }
   }
 
-  function hideDefaultPluginCopy($cardMethod) {
-    $cardMethod.find('.payment_box > p').each(function () {
+  function hideDefaultCardStuff($cardMethod) {
+    var $paymentBox = $cardMethod.children('.payment_box').first();
+
+    $paymentBox.children('p').each(function () {
       var $p = $(this);
       var text = normalizeText($p.text());
 
       if (
-        text.indexOf('charges on your card statement') !== -1 ||
+        text.indexOf('charges on your card') !== -1 ||
         text.indexOf('lupa group') !== -1 ||
         text.indexOf('international payments') !== -1 ||
-        text.indexOf('transaction failure') !== -1 ||
-        text.indexOf('card statement') !== -1
+        text.indexOf('card payment') !== -1 ||
+        text.indexOf('secure card') !== -1
       ) {
-        $p.addClass('axiom-card-default-copy-hidden');
+        $p.addClass('axiom-card-default-hidden');
       }
     });
 
-    $cardMethod.find('.payment_box > img').addClass('axiom-card-default-icons-hidden');
-    $cardMethod.find('.payment_box > .card-icons').addClass('axiom-card-default-icons-hidden');
-    $cardMethod.find('.payment_box > .payment-icons').addClass('axiom-card-default-icons-hidden');
-    $cardMethod.find('.payment_box > .accepted-cards').addClass('axiom-card-default-icons-hidden');
-  }
+    $paymentBox.children('img, svg, picture').addClass('axiom-card-default-hidden');
 
-  function addMethodClass($cardMethod) {
-    if ($cardMethod.length) {
-      $cardMethod.addClass('axiom-card-method-detected');
-    }
+    $paymentBox.find('img').each(function () {
+      var src = normalizeText($(this).attr('src'));
+      var alt = normalizeText($(this).attr('alt'));
+
+      if (
+        src.indexOf('visa') !== -1 ||
+        src.indexOf('master') !== -1 ||
+        src.indexOf('amex') !== -1 ||
+        src.indexOf('discover') !== -1 ||
+        alt.indexOf('visa') !== -1 ||
+        alt.indexOf('master') !== -1 ||
+        alt.indexOf('amex') !== -1 ||
+        alt.indexOf('discover') !== -1
+      ) {
+        $(this).addClass('axiom-card-default-hidden');
+      }
+    });
   }
 
   function insertCardPanel() {
@@ -191,8 +165,7 @@
       return;
     }
 
-    addMethodClass($cardMethod);
-    removeDuplicateCardPanels($cardMethod);
+    removeDuplicatePanels($cardMethod);
 
     var $paymentBox = $cardMethod.children('.payment_box').first();
 
@@ -204,11 +177,11 @@
       $paymentBox.prepend(buildCardPanel());
     }
 
-    hideDefaultPluginCopy($cardMethod);
+    hideDefaultCardStuff($cardMethod);
   }
 
   function refreshCardPanel() {
-    insertCardPanel();
+    setTimeout(insertCardPanel, 20);
   }
 
   $(document).ready(function () {
@@ -220,11 +193,7 @@
   });
 
   $(document).on('change', '.woocommerce-checkout #payment ul.payment_methods > li > input.input-radio, .woocommerce-checkout #payment ul.payment_methods > li > input[type="radio"]', function () {
-    setTimeout(refreshCardPanel, 25);
+    refreshCardPanel();
   });
-
-  setTimeout(refreshCardPanel, 250);
-  setTimeout(refreshCardPanel, 750);
-  setTimeout(refreshCardPanel, 1500);
 
 })(jQuery);
